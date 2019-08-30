a726c615332ecb8bc43373d649080864a6242804
ty <adidasty@gmail.com>
2019-08-30 17:16:53 +0900

    two mode PCS tracked

--- two_mode_pcs.py
+++ two_mode_pcs.py
@@ -4,7 +4,7 @@ import scipy as sp
 from scipy import sparse
 from scipy.sparse import csr_matrix,coo_matrix
 
-class Pairfrag:
+class Pcs:
     def __init__(self,numPtcl):
         self.N=numPtcl
         N=self.N
@@ -28,27 +28,27 @@ class Pairfrag:
         adata=[]
         for k in range(N+1):
             for j in range(N-1):
-                acol.append(g[j,k])
-                arow.append(g[j+2,k])
-                adata.append(np.sqrt(j+1)*np.sqrt(j+2))
+                    acol.append(g[j,k])
+                    arow.append(g[j+2,k])
+                    adata.append(np.sqrt(j+1)*np.sqrt(j+2))
         ## c corresponds to a_{0}^{*}a_{1}^{*}
         crow=[]
         ccol=[]
         cdata=[]
         for k in range(N):
             for j in range(N):
-                ccol.append(g[k,j])
-                crow.append(g[k+1,j+1])
-                cdata.append(np.sqrt(j+1)*np.sqrt(k+1))
+                    ccol.append(g[k,j])
+                    crow.append(g[k+1,j+1])
+                    cdata.append(np.sqrt(j+1)*np.sqrt(k+1))
         ## d corresponds to a_{1}^{*2}
         drow=[]
         dcol=[]
         ddata=[]
         for k in range(N+1):
             for j in range(N-1):
-                dcol.append(g[k,j])
-                drow.append(g[k,j+2])
-                ddata.append(np.sqrt(j+1)*np.sqrt(j+2))
+                    dcol.append(g[k,j])
+                    drow.append(g[k,j+2])
+                    ddata.append(np.sqrt(j+1)*np.sqrt(j+2))
 
         arow=np.int_(np.asarray(arow))
         acol=np.int_(np.asarray(acol) )
@@ -59,38 +59,26 @@ class Pairfrag:
         adata=np.asarray(adata)
         cdata=np.asarray(cdata)
         ddata=np.asarray(ddata)
-
-        AA=csr_matrix((adata, (arow, acol)), shape=((N+1)**2, (N+1)**2))
-        CC=csr_matrix((cdata, (crow, ccol)), shape=((N+1)**2, (N+1)**2))
-        DD=csr_matrix((ddata, (drow, dcol)), shape=((N+1)**2, (N+1)**2))
-
+        AA=coo_matrix((adata, (arow, acol)), shape=((N+1)**2, (N+1)**2)).tocsr()
+        CC=coo_matrix((cdata, (crow, ccol)), shape=((N+1)**2, (N+1)**2)).tocsr()
+        DD=coo_matrix((ddata, (drow, dcol)), shape=((N+1)**2, (N+1)**2)).tocsr()
         H=((np.sin(theta1)*np.cos(theta2)*(1/np.sqrt(2))*AA)+(np.cos(theta1)*CC)+(np.sin(theta1)*np.sin(theta2)*(1/np.sqrt(2))*DD))
         return H
 
-    def state(self,theta1,theta2,theta3,theta4,m):
+    def pcsstate(self,theta1,theta2):
         N=self.N
         g=self.g
-        mm=m
-        if mm>N/4:
-            return print('m must be less than or equal to N/4')
-        else:
 
-            ms=np.zeros((N+1)**2)
-            ms[0]=1
+        ms=np.zeros((N+1)**2)
+        ms[0]=1
 
-            xx=ms
-            #Apply the factors in alternation up to the number defining the smallest fragment
-            for j in range(mm):
-                xx=( self.A(theta1,theta2).dot(xx) )/np.linalg.norm(xx)
-                xx=( self.A(theta3,theta4).dot(xx) )/np.linalg.norm(xx)
-            #Apply the remaining factors
-            for j in range(np.int((N/2)-(2*mm))):
-                xx=( self.A(theta1,theta2).dot(xx) )/np.linalg.norm(xx)
+        xx=ms
+        for j in range(np.int(N/2)):
+            xx=( self.A(theta1,theta2).dot(xx) )/np.linalg.norm(xx)
+        state=[]
 
-            state=[]
-
-            for k in range(N+1):
-                state.append(xx[np.int(g[N-k,k])])
-            state=np.asarray(state)
-            state=state/np.linalg.norm(state)
+        for k in range(N+1):
+            state.append(xx[np.int(g[N-k,k])])
+        state=np.asarray(state)
+        state=state/np.linalg.norm(state)
         return state
